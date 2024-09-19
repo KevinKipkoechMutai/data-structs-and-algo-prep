b92427e20b25d7fd6e45a39504a442ee6cb841d8
kevinmutai <mutaikevinkip.05@gmail.com>
2024-09-19 13:19:17 +0300

    divisible sum pairs

---
+++ divisible-sum-pairs/divisible-sum-pairs.py
@@ -0,0 +1,10 @@
+def sumPairs(n, k, arr):
+    # n = len(arr)
+    count = 0
+
+    for i in range(n):
+        for j in range(i+1, n):
+            if (arr[i] + arr[j]) % k == 0:
+                count += 1
+    
+    return count
